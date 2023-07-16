3dbb0aa3b682576a9a3d0428a4044597750c5fff
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-16 19:03:45 +0100

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,5 +10,7 @@ if number > 0 and int(new_number[-1]) != 0:
         print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, new_number[-1]))
 elif (number < 0 and int(new_number[-1]) == 0) or (number > 0 and int(new_number[-1]) == 0) :
         print("Last digit of {0} is {1} and is 0".format(number, new_number[-1]))
+elif number == 0:
+    print("Last digit of {0} is {1} and is 0".format(number, number))
 else:
     print("Last digit of {0} is -{1} and is less than 6 and not 0".format(number, new_number[-1]))
