ba4c7297e0cca45c8f351e9ec7c33b5f877d856d
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-21 00:33:09 +0100

    String manipulation function

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,11 +1,13 @@
 #!/urs/bin/python3
 def reverse_string(string):
-    word = []
-    for i in range(len(string)):
-        word.append(string[(len(string) - i) - 1])
-    reversed_string = ''.join(word)
-    return reversed_string
-    pass
+    return string[::-1]
 
 name = "chijioke"
 reverse_string(name)
+
+# def reverse_string(string):
+#     word = []
+#     for i in range(len(string)):
+#         word.append(string[(len(string) - i) - 1])
+#     reversed_string = ''.join(word)
+#     return reversed_string
