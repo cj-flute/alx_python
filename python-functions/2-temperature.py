14d865eac63e50207711cc1c053760a463c2a067
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-21 22:26:06 +0100

    Temperature converter function

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,6 +3,7 @@ def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
 
+
 print(convert_to_celsius(100))
 print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
