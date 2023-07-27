15d3738c8a927a6c4e3ac48c8f4649241b1ed1de
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-28 00:34:21 +0100

    arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,17 @@
 # 1/usr/bin/python3
-
 import sys
 
 
-class Aguments:
-    count = 0
+def out_arguments(argv):
+    number_of_arguments = len(argv) - 1
+    plural = "s" if number_of_arguments != 1 else ""
 
-    def __init__(self, nums):
-        self.name = nums
-        count += 1
-
-    @staticmethod
-    def num_of_args():
-        print("{0}: {1}".format(count, dir()))
+    print(f"{number_of_arguments}: argument{plural}", end="")
+    if number_of_arguments > 0:
+        print()
+        for i, arg in enumerate(argv[1:], start=1):
+            print(f"{i}: {arg}")
 
 
-Aguments(3)
+if __name__ == "__main__":
+    out_arguments(sys.argv)
