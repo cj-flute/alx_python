174a87f4276f623d42de0d450c17f34a91d33adb
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-28 13:06:34 +0100

    Raise a message

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def raise_exception_msg():
+    value = "Hello"
+    try:
+        print(val)
+    except NameError:
+        print("C is fun")
+    pass
+
+
+def main():
+    raise_exception_msg()
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    pass
