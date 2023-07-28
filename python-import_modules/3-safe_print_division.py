51fda87c0ae75016a572c7cc176a030cd85002ba
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-28 12:18:19 +0100

    Integer division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -24,17 +24,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# a = 12
-# b = 0
-# try:
-#     result = safe_print_division(a, b)
-#     print("{:d} / {:d} = {}".format(a, b, result))
-#     pass
-# except ZeroDivisionError:
-#     result = "None"
-#     print("{:d} / {:d} = None".format(a, b))
-#     pass
-# finally:
-#     print("Inside result: {}".format(result))
-#     pass
