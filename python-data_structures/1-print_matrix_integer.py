0625ba9f9ca3da12a538a7d3d016afaad84885b7
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-31 13:49:12 +0100

    Lists of lists

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            matrix[i][0], matrix[i][1], matrix[i][2] = matrix[i]
+            if j < 2:
+                print("{}".format(matrix[i][j]), end=" ")
+            elif j == 2:
+                print("{}".format(matrix[i][j]), end="")
+        print(end="$\n")
+    pass
+
+
+def main():
+    print_matrix_integer(matrix)
+    print("--", end="$\n")
+    print_matrix_integer()
+    pass
+
+
+if __name__ == "__main__":
+    main()
