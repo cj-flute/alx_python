863bad81aaee16b9c2dbe7969b8f1a7d5b28e83d
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-02 20:08:36 +0100

    Square with size

---
+++ python-classes/0-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+
+def main():
+    my_sqaure = Square(3)
+    print(type(my_sqaure))
+    print(my_sqaure.__dict__)
+
+    try:
+        print(my_sqaure.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_sqaure.__size)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
