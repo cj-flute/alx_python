c9a892406bd8afbd0701e42cc9e53262e5364fe6
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-04 13:51:14 +0100

    square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 '''
-python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("0-square").__doc__)'
 '''
 
 
 class Square:
     '''
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    python3 -c 'print(__import__("0-square").Square.__doc__)'
     '''
 
     def __init__(self, size):
         '''
-        python3 -c 'print(__import__("my_module").my_function.__doc__)' and 
-        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        python3 -c 'print(__import__("0-square").__init__.__doc__) 
+        python3 -c 'print(__import__("0-square").Square.__init.__doc__)
         '''
         self.__size = size
 
@@ -21,12 +21,12 @@ my_sqaure = Square(3)
 print(type(my_sqaure))
 print(my_sqaure.__dict__)
 
-# try:
-#     print(my_sqaure.size)
-# except Exception as e:
-#     print(e)
+try:
+    print(my_sqaure.size)
+except Exception as e:
+    print(e)
 
-# try:
-#     print(my_sqaure.__size)
-# except Exception as e:
-#     print(e)
+try:
+    print(my_sqaure.__size)
+except Exception as e:
+    print(e)
