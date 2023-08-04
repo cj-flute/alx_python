3c1c2188b69c5229e1b9cce141b5a56cbf18d10d
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-04 14:46:13 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
 '''
-python3 -c 'print(__import__("0-square").__doc__)'
+    python3 -c 'print(__import__("1-square").__doc__)
 '''
 
 
 class Square:
     '''
-    python3 -c 'print(__import__("0-square").Square.__doc__)'
+        python3 -c 'print(__import__("1-square").Square.__doc__)
     '''
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         '''
-        python3 -c 'print(__import__("0-square").__init__.__doc__) 
-        python3 -c 'print(__import__("0-square").Square.__init.__doc__)
+            python3 -c 'print(__import__("0-square").__init__.__doc__)
+            python3 -c 'print(__import__("0-square").Square.__init__.__doc__)
         '''
         self.__size = size
-
-
-my_sqaure = Square(3)
-print(type(my_sqaure))
-print(my_sqaure.__dict__)
-
-try:
-    print(my_sqaure.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_sqaure.__size)
-except Exception as e:
-    print(e)
