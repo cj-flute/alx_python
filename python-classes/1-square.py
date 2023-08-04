852483e7243d8a415388c0b4f402e47a831b9980
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-04 12:24:35 +0100

    Size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+    python3 -c 'print(__import__("1-square").__doc__)
+'''
+
+
+class Square:
+    '''
+        python3 -c 'print(__import__("1-square").Square.__doc__)
+    '''
+
+    def __init__(self, size=0):
+        '''
+            python3 -c 'print(__import__("1-square").__init__.__doc__)
+            python3 -c 'print(__import__("1-square").Square.__init__.__doc__)
+        '''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
