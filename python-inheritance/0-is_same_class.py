7a4ad3c5c2d1e9723fcda7799b94ff62282297f9
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-05 00:45:26 +0100

    Exact same object

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -20,6 +20,15 @@ class A:
 
 
 def is_same_class(obj, a_class):
+    '''
+        is_same_class(obj, a_class):
+            if isinstance(obj, a_class):
+                return True
+            elif isinstance(obj, int):
+                return "{} is an instance of the class int".format(obj)
+            else:
+                return False
+    '''
     if isinstance(obj, a_class):
         return True
     elif isinstance(obj, int):
