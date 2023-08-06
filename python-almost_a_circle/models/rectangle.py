a7cbd156f6e5a97abaae1d6c7ef22386aac1e2e0
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-06 20:35:42 +0100

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,33 +1,8 @@
 #!/usr/bin/python3
+from base import Base
 '''
-    models module
-    Holds class Base and Rectangle
     Rectangle class inherits from base class
 '''
-
-
-class Base:
-    '''models/base
-        Base class with private class attribute __nb_object = 0
-        def __init__(self, id=None):
-            if id != None:
-                self.id = id
-            else:
-                Base.__nb_objects += 1
-                self.id = Base.__nb_objects
-    '''
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        '''
-            Base.__init__()
-            instance id
-        '''
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
 
 
 class Rectangle(Base):
