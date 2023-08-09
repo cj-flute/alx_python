324346db52395820122dc81f33e89a960fce1cd6
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-09 02:02:52 +0100

    Display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -146,7 +146,7 @@ class Rectangle(Base):
         for i in range(self.__x):
             print(end="")
         for i in range(self.__height):
-            if self.__y == 0:
+            if self.__y == 0 and self.__x > 0:
                 self.__y = 1
             print(" " * self.__y, end="")
             print(char * self.__width)
