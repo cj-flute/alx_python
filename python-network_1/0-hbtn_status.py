cc69786abf4ab198c2a06b11ac3022ed1125f82b
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-14 20:41:21 +0100

    python neetwor first task with readme

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+    python3 -c 'print(__import__("requests").__doc__)'
+'''
+import requests
+
+
+def main():
+    '''
+        python3 -c 'print(__import__("requests").my_function.__doc__)'
+        main function
+    '''
+    r = requests.get('https://alu-intranet.hbtn.io/status')
+
+    print("- type: {}".format(type(str(r._content))))
+    print("- content: {}".format(r.content.decode()))
+
+
+if __name__ == "__main__":
+    main()
