f4ae85c7a015958b2be42ef1f28813bb85109b22
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-14 21:10:01 +0100

    Response header value

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+    python3 -c 'print(__import__("requests").__doc__)'
+'''
+import requests
+import sys
+
+
+def main():
+    '''
+        python3 -c 'print(__import__("requests").main().__doc__)'
+        main function
+    '''
+    r = input()
+    the_url = requests.get(r)
+    if 'X-Request-ID' in the_url.headers:
+        x_request_id = the_url.headers['X-Request-ID']
+        print(x_request_id)
+
+
+if __name__ == "__main__":
+    main()
