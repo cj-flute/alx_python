29a0a80d16ab7fbfde88a49edc86f788ff8a1214
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-22 19:35:38 +0100

    Response hearder value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -11,7 +11,10 @@ def main():
         python3 -c 'print(__import__("requests").main().__doc__)'
         main function
     '''
-    r = input()
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    r = sys.argv[1]
     the_url = requests.get(r)
     if 'X-Request-ID' in the_url.headers:
         x_request_id = the_url.headers['X-Request-ID']
