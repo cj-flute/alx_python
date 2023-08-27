f7cd67b39dee94aded3b890592d669518dc12be2
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-27 09:17:17 +0100

    only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -5,12 +5,3 @@ def inherits_from(obj, a_class):
         return True
     else:
         return False
-
-
-a = True
-if inherits_from(type(a), int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(type(a), bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(type(a), object):
-    print("{} inherited from class {}".format(a, object.__name__))
