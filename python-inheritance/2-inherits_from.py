9520ab7ac4fe8241a5ca6620f8960e7ef26af640
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-27 09:35:07 +0100

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+'''2-inherit_form.py'''
+
 
 def inherits_from(obj, a_class):
-    if issubclass(obj, a_class):
-        return True
-    else:
-        return False
+    """inherit_from() fuction"""
+    inherited_classses = type(obj).mro()
+    return a_class in inherited_classses
