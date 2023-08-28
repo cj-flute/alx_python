aa4619de1ec4d5092efa6a8d9da6077e1a7e8231
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-28 09:12:34 +0100

    Integer validator

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -16,7 +16,27 @@ class BaseGeometry:
         self.value = value
 
         if type(self.value) != int:
-            raise TypeError("{} must be an integer".format(__class__.__name__))
+            raise TypeError("must be an integer")
         elif self.value <= 0:
-            raise ValueError(
-                "{} must be greater than 0".format(__class__.__name__))
+            raise ValueError("must be greater than 0")
+
+
+bg = BaseGeometry()
+
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
