19f6c0423f2b1d32c59181d002970b865fbc5bb6
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-28 09:09:12 +0100

    Integer Validator

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/urs/bin/python3
+
+'''3-base_geometry.py'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry class      
+    '''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if type(self.value) != int:
+            raise TypeError("{} must be an integer".format(__class__.__name__))
+        elif self.value <= 0:
+            raise ValueError(
+                "{} must be greater than 0".format(__class__.__name__))
