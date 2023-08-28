71ffc8ec94fc758a2c2dd700cc7edfbc930e4fd7
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-28 08:15:53 +0100

    Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,12 @@
+#!/urs/bin/python3
+
+'''3-base_geometry.py'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry class      
+    '''
+
+    def area(self):
+        raise Exception("area() is not implemented")
