99b3975dfd545648ee2deada39600e48d961803f
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-28 10:52:30 +0100

    Rectangle

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,38 @@
+#!/urs/bin/python3
+
+'''3-base_geometry.py'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry class      
+    '''
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if type(self.value) != int:
+            raise TypeError("{} must be an integer".format(self.name))
+        elif self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        w = self.__width
+        h = self.__height
+
+        if w < 0:
+            raise TypeError("width must be positive integer")
+        elif type(w) != int:
+            raise TypeError("width must be an integer")
+        if h < 0:
+            raise TypeError("heigth must be positive integer")
+        elif type(h) != int:
+            raise TypeError("heigth must be an integer")
