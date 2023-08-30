0a5572f3f467fc4f0680efac76235bb35a68004d
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-08-30 03:40:23 +0100

    My GitHub

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -17,7 +17,8 @@ def main():
     username = sys.argv[1]
     password = sys.argv[2]
     url = 'https://api.github.com/user'
-    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    data = (username, personal_access_token)
+    response = requests.get(url, auth=data)
     json_response = response.json()
     print(json_response.get("id"))
     pass
