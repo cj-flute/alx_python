ae8dc39dfa27be8b1969271ade30c7618b3ef985
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-01 17:33:42 +0100

    Prime Number Function

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def is_prime(num):
-    if (num == 1 or num == 2 or num == 3
-            or num == 5 or num == 7 or
-            num == 11):
-        return True
-    if num <= 0:
-        return False
-    if (num % 2 != 0 and num % 3 != 0
-            and num % 5 != 0 and
-            num % 7 != 0 and
-            num % 11 != 0):
-        return True
+    if type(num) == int:
+        if (num == 1 or num == 2 or num == 3
+                or num == 5 or num == 7 or
+                num == 11):
+            return True
+        if num <= 0:
+            return False
+        if (num % 2 != 0 and num % 3 != 0
+                and num % 5 != 0 and
+                num % 7 != 0 and
+                num % 11 != 0):
+            return True
+        else:
+            return False
     else:
         return False
     pass
