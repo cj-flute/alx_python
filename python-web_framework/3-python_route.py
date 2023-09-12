cd397edf5005f5fe7f183b5ddb42b17441ea1c53
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-12 02:02:57 +0100

    Python is cool

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,39 @@
+"""
+    A script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def texts(text):
+    word = []
+    text_list = list(text)
+    for texts in range(len(text)):
+        if text_list[texts] == '_':
+            text_list[texts] = ' '
+        word.append(text_list[texts])
+    words = ''.join(word)
+    return f'C {words}'
+
+
+@app.route('/python/<text>')
+def python(text):
+    new_text = texts(text)
+    return 'Python' + new_text[1:]
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
