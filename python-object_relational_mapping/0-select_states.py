157c9291ecd3d287b4e8215ba9c70b2a22106e24
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-10-16 01:24:34 +0100

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,27 @@
 import MySQLdb as DB
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 db_connect = DB.connect(host='localhost', port=3306,
                         user='root', passwd='mummysdaycjflute', db='cj')
 
 db_cursor = db_connect.cursor()
 
-db_cursor.execute("CREATE DATABASE IF NOT EXITS hbtn_0e_0_usa;")
+db_cursor.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;")
 db_cursor.execute("USE hbtn_0e_0_usa;")
 db_cursor.execute(
-    """CREATE TABLE IF NOT EXISTS state(
+    """CREATE TABLE IF NOT EXISTS states(
         id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(256) NOT NULL,
         PRIMARY KEY (id)
     )"""
 )
+
+states = ("Arizona", "Califonia", "Neveda", "New york", "Texas")
+
+for state in states:
+    db_cursor.execute("INSERT INTO states (name) VALUES (%s)", [state])
+
+db_connect.commit()
+db_cursor.close()
+
 # db_cursor.execute("INSERT INTO states ()")
