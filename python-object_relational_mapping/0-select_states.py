3d679396a603e074a1789cd428fe423e90696220
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-10-17 00:49:14 +0100

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,30 +9,31 @@ db_connect = DB.connect(
     passwd=sys.argv[2],
     db=sys.argv[3])
 
-db_cursor = db_connect.cursor()
 
-db_cursor.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;")
-db_cursor.execute("USE hbtn_0e_0_usa;")
-db_cursor.execute(
-    """CREATE TABLE IF NOT EXISTS states(
-        id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(256) NOT NULL,
-        PRIMARY KEY (id)
-    )"""
-)
+if __name__ == '__main__':
+    db_cursor = db_connect.cursor()
 
+    db_cursor.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;")
+    db_cursor.execute("USE hbtn_0e_0_usa;")
+    db_cursor.execute(
+        """CREATE TABLE IF NOT EXISTS states(
+            id INT NOT NULL AUTO_INCREMENT,
+            name VARCHAR(256) NOT NULL,
+            PRIMARY KEY (id)
+        )"""
+    )
 
-states = ("California", "Arizona", "Texas", "New york", "Nevada")
+    states = ("California", "Arizona", "Texas", "New york", "Nevada")
 
-for state in states:
-    db_cursor.execute("INSERT INTO states (name) VALUES (%s)", [state])
+    for state in states:
+        db_cursor.execute("INSERT INTO states (name) VALUES (%s)", [state])
 
-db_cursor.execute("SELECT * FROM states ORDER BY id")
+    db_cursor.execute("SELECT * FROM states ORDER BY id")
 
-row_sellected = db_cursor.fetchall()
+    row_sellected = db_cursor.fetchall()
 
-for row in row_sellected:
-    print(row)
+    for row in row_sellected:
+        print(row)
 
-db_connect.commit()
-db_cursor.close()
+    db_connect.commit()
+    db_cursor.close()
