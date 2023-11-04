479ca5834e416ea3770445b4874b744191ad7f09
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-11-04 01:01:47 +0100

    SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+import MySQLdb as DB
+import sys
+
+user_name = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+state_name = sys.argv[4]
+
+db_connect = DB.connect(
+    host='localhost',
+    port=3306,
+    user=user_name,
+    passwd=password,
+    db=db_name)
+
+
+if __name__ == '__main__':
+    db_cursor = db_connect.cursor()
+
+    sql_query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        state_name)
+
+    db_cursor.execute(sql_query)
+
+    row_sellected = db_cursor.fetchall()
+
+    for row in row_sellected:
+        print(row)
+
+    db_connect.commit()
+    db_cursor.close()
+    db_connect.close()
