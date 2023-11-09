0570fe8d947fc7f0b4579f0c38bb4eab4cf8b044
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-11-09 11:07:38 +0100

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a=0, b=0):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        print("{:d} / {:d} = {}".format(a, b, result))
+        # print("{:d} / {:d} = {}".format(a, b, result))
         return result
 
 
